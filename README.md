3bd9d62acbf9a2f2ee0582308ac540c6e03c9ca5
Maja Grys <majusiag@poczta.onet.pl>
2023-10-01 21:23:42 +0200

    Added the website

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Random-Facts
+# Random Facts
+This is a simple website built during a web development course to practice the implementation of responsive web design and API. It allows users to receive a random fact from one of four categories.
+
+The API used during this project include:
+* [Dog API](https://dogapi.dog/)
+* [MeowFacts API](https://github.com/wh-iterabb-it/meowfacts)
+* [Numbers API](http://numbersapi.com/)
+* [Useless Facts API](https://uselessfacts.jsph.pl/)
---
+++ script.js
@@ -0,0 +1,19 @@
+function handleDogs() {
+    fetch('https://dog-api.kinduff.com/api/facts?number=1')
+    .then(res => res.json())
+    .then(data => document.getElementById('fact').textContent = data.facts)}
+
+function handleCats() {
+    fetch('https://meowfacts.herokuapp.com/')
+    .then(res => res.json())
+    .then(data => document.getElementById('fact').textContent = data.data)}
+
+function handleNumbers() {
+    fetch('http://numbersapi.com/random/trivia?json')
+    .then(res => res.json())
+    .then(data => document.getElementById('fact').textContent = data.text)}
+
+function handleOther() {
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+    .then(res => res.json())
+    .then(data => document.getElementById('fact').textContent = data.text)}
